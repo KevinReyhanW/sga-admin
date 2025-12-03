79d4f89ff3ab9e3956c690305a65f17c8a70091d
Harris <zevenpixel@gmail.com>
2025-12-03 09:37:10 +0700

    Add branding to login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,9 @@ import LoginForm from "@/app/login/login-form";
 
 function Page() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-gradient-to-b from-blue-50 via-blue-100 to-blue-200">
+    <div className="flex flex-col min-h-svh w-full items-center justify-center p-6 md:p-10 bg-gradient-to-b from-blue-50 via-blue-100 to-blue-200">
+      <img src="/logo-sga.png" alt="logo" className="w-24 mb-5" />
+      <h3 className="text-2xl font-semibold mb-5">Smart Guest Assignement</h3>
       <div className="w-full max-w-sm">
         <LoginForm />
       </div>
