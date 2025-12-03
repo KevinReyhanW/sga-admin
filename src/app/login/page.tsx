9e3b1690a1038d268e17af2034fd0be97799b49c
Harris <zevenpixel@gmail.com>
2025-12-03 09:49:00 +0700

    Fix some typo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import LoginForm from "@/app/login/login-form";
 
@@ -5,7 +6,7 @@ function Page() {
   return (
     <div className="flex flex-col min-h-svh w-full items-center justify-center p-6 md:p-10 bg-gradient-to-b from-blue-50 via-blue-100 to-blue-200">
       <img src="/logo-sga.png" alt="logo" className="w-24 mb-5" />
-      <h3 className="text-2xl font-semibold mb-5">Smart Guest Assignement</h3>
+      <h3 className="text-2xl font-semibold mb-5">Smart Guest Assignment</h3>
       <div className="w-full max-w-sm">
         <LoginForm />
       </div>
